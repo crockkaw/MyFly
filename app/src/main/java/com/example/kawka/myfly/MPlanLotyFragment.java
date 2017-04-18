aa3a5c61e5762d3dac2c9c5bfdf0e8b870b72281
crockkaw <crockkaw@gmail.com>
2017-04-19 00:26:03 +0200

    update layout 19.04

--- app/src/main/java/com/example/kawka/myfly/MPlanLotyFragment.java
+++ app/src/main/java/com/example/kawka/myfly/MPlanLotyFragment.java
@@ -1,5 +1,7 @@
 package com.example.kawka.myfly;
 
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.annotation.Nullable;
@@ -8,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import com.loopeer.android.librarys.scrolltable.ScrollTableView;
@@ -24,6 +27,23 @@ public class MPlanLotyFragment extends Fragment {
     private  String[] topTitles;
     private static final String[] leftTitles = new String[] {" ", " ", " "};
 
+    TextView nalotTextView1,nalotTextView2,nalotTextView3,nalotTextView4;
+
+    String[] row11 = new String[] {"08:40","09:55","-","W-3"};
+    String[] row21 = new String[] {"11:10","11:35","4","W-3"};
+    String[] row31 = new String[] {"13:25","14:10","2","MI-17"};
+
+    String[] row12 = new String[] {"09:00","10:00","-","W-3"};
+    String[] row22 = new String[] {"11:25","12:10","2","Mi-17"};
+    String[] row32 = new String[] {"13:10","14:35","4","MI-17"};
+
+    String[] row13 = new String[] {"08:15","09:55","1","W-3"};
+    String[] row23 = new String[] {"11:10","11:35","3","MI-17"};
+    String[] row33 = new String[] {"12:00","13:00","-","MI-17"};
+
+    String[] row14 = new String[] {"08:30","09:30","-","MI-17"};
+    String[] row24 = new String[] {"10:00","10:25","3","MI-17"};
+    String[] row34 = new String[] {"11:00","14:40","1","W-3"};
 
 
 
@@ -46,11 +66,32 @@ public class MPlanLotyFragment extends Fragment {
         ArrayList<String> topTitles = createTopTitles();
 
 
-        stv_wariant1.setDatas(createTopTitles(), createLeftTitles(), createContent(3, topTitles.size()));
-        stv_wariant2.setDatas(createTopTitles(), createLeftTitles(), createContent(3, topTitles.size()));
-        stv_wariant3.setDatas(createTopTitles(), createLeftTitles(), createContent(3, topTitles.size()));
-        stv_wariant4.setDatas(createTopTitles(), createLeftTitles(), createContent(3, topTitles.size()));
+        stv_wariant1.setDatas(createTopTitles(), createLeftTitles(), createContent(row11,row21,row31));
+        stv_wariant2.setDatas(createTopTitles(), createLeftTitles(), createContent(row12,row22,row32));
+        stv_wariant3.setDatas(createTopTitles(), createLeftTitles(), createContent(row13,row23,row33));
+        stv_wariant4.setDatas(createTopTitles(), createLeftTitles(), createContent(row14,row24,row34));
 
+        nalotTextView1 = (TextView) myView.findViewById(R.id.nalotTextView1);
+        nalotTextView2 = (TextView) myView.findViewById(R.id.nalotTextView2);
+        nalotTextView3 = (TextView) myView.findViewById(R.id.nalotTextView3);
+        nalotTextView4 = (TextView) myView.findViewById(R.id.nalotTextView4);
+
+
+        java.util.Calendar instance = java.util.Calendar.getInstance();
+        String dd = String.valueOf(instance.get(java.util.Calendar.DAY_OF_MONTH));
+        String mm = String.valueOf(instance.get(java.util.Calendar.MONTH)+1);
+        String yy = String.valueOf(instance.get(java.util.Calendar.YEAR));
+        String date = dd + "." + mm + "." + yy;
+
+        String dd2 = String.valueOf(instance.get(java.util.Calendar.DAY_OF_MONTH)+1);
+        String date2 = dd2 + "." + mm + "." + yy;
+
+
+
+        nalotTextView1.setText("PTL WARIANT I / "+date);
+        nalotTextView2.setText("PTL WARIANT II / "+date);
+        nalotTextView3.setText("PTL WARIANT I / "+date2);
+        nalotTextView4.setText("PTL WARIANT II / "+date2);
 
         return myView;
     }
@@ -60,15 +101,29 @@ public class MPlanLotyFragment extends Fragment {
 
 
 
-    private ArrayList<ArrayList<String>> createContent(int row, int column) {
+    private ArrayList<ArrayList<String>> createContent(String[] row1, String[] row2, String[] row3 ) {
+
+
         ArrayList<ArrayList<String>> results = new ArrayList<>();
-        for (int i = 0; i < row; i++) {
-            ArrayList<String> strings = new ArrayList<>();
-            for (int j = 0; j < column; j++) {
-                strings.add( i + "" + j);
+            ArrayList<String> strings1 = new ArrayList<>();
+            for (String string : row1) {
+                strings1.add(string);
             }
-            results.add(strings);
-        }
+            results.add(strings1);
+
+            ArrayList<String> strings2 = new ArrayList<>();
+            for (String string : row2) {
+                strings2.add(string);
+            }
+            results.add(strings2);
+
+            ArrayList<String> strings3 = new ArrayList<>();
+            for (String string : row3) {
+                strings3.add(string);
+            }
+            results.add(strings3);
+
+
         return results;
     }
 
